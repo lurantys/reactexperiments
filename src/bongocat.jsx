53c5b1f618bb9fee7fc7c6b984767ee5922a6d2f
Aymen <a.nhaila@aui.ma>
2025-01-15 15:25:20 +0100

    bug fix

--- src/bongocat.jsx
+++ src/bongocat.jsx
@@ -1,22 +1,25 @@
+import { useState, useEffect } from 'react';
 import bongoCat from './assets/cat1.png';
 import bongoCat2 from './assets/cat2.png';
-import textbox from "./typeholder.jsx";
-const textInput = document.getElementById("textbox");
-const bongo = document.querySelector(".bongo-cat");
-let isCat1 = true;
-textInput.addEventListener("input", () => {
-  if (isCat1) {
-    bongo.innerHTML = `<img src=${bongoCat2} alt="Bongo Cat" />`;
-  } else {
-    bongo.innerHTML = `<img src=${bongoCat} alt="Bongo Cat" />`;
-  }
-  isCat1 = !isCat1;
-});
+
 function BongoCat() {
+  const [isCat1, setIsCat1] = useState(true);
+
+  useEffect(() => {
+    const textInput = document.getElementById("textbox");
+    const handleInput = () => {
+      setIsCat1(prev => !prev);
+    };
+    
+    textInput?.addEventListener("input", handleInput);
+    return () => textInput?.removeEventListener("input", handleInput);
+  }, []);
+
   return (
     <div className='bongo-cat'>
-      <img src={bongoCat} alt="Bongo Cat" />
+      <img src={isCat1 ? bongoCat : bongoCat2} alt="Bongo Cat" />
     </div>
   );
 }
+
 export default BongoCat;
